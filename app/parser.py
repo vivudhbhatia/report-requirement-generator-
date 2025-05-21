6c9def110f0f796274af9885cd8cd89ef50265b5
vivudhbhatia <vivudhbhatia@gmail.com>
2025-05-21 16:24:24 -0400

    Update parser.py

--- app/parser.py
+++ app/parser.py
@@ -1,1 +1,7 @@
+import pdfplumber
+
+def parse_pdf(uploaded_file):
+    with pdfplumber.open(uploaded_file) as pdf:
+        text = "\n".join([page.extract_text() for page in pdf.pages if page.extract_text()])
+    return text
 
