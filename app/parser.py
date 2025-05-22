83201cd3f594290391fae9ada022ac17d4dd63b8
vivudhbhatia <vivudhbhatia@gmail.com>
2025-05-22 14:21:48 -0400

    Update parser.py

--- app/parser.py
+++ app/parser.py
@@ -9,11 +9,4 @@ def parse_pdf_from_file(file):
         doc = fitz.open(tmp.name)
         text = "\n".join([page.get_text() for page in doc])
         title_text = doc[0].get_text()
-        report_id = "UNKNOWN"
-        if "Y-9C" in title_text:
-            report_id = "FR Y-9C"
-        elif "FFIEC 002" in title_text:
-            report_id = "FFIEC 002"
-        elif "FFIEC 009" in title_text:
-            report_id = "FFIEC 009"
-        return text, report_id
+        return text, title_text
